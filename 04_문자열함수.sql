600925d64b325cd5c3af6a30e7807df45134fd96
hansolL <ilsysli@naver.com>
2023-09-22 17:40:52 +0900

    23. 09. 22. 수업  연산/형변환/그룹

--- 04_문자열함수.sql
+++ 04_문자열함수.sql
@@ -96,7 +96,7 @@ EMPLOYEES 테이블에서 phone_number컬럼은 ###.###.####형태로 저장되어 있다
 전화 번호를 출력하도록 쿼리를 작성하세요. (CONCAT, SUBSTR, LENGTH 사용)
 */
 SELECT
-    CONCAT('(02)',SUBSTR(phone_number, 4))
+    CONCAT('(02)',SUBSTR(phone_number, 5))
 FROM employees;
 
 /*
@@ -110,6 +110,6 @@ EMPLOYEES 테이블에서 JOB_ID가 it_prog인 사원의 이름(first_name)과 급여(salary)를
 */
 SELECT
     RPAD(SUBSTR(first_name, 1, 3),LENGTH(first_name), '*') AS name,
-    RPAD(salary, 10, '*') AS salary
+    LPAD(salary, 10, '*') AS salary
 FROM employees
-WHERE LOWER(job_id)='it_prog';
+WHERE LOWER(job_id) = 'it_prog';
